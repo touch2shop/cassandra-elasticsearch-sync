a9dae66a0fed60c2d2c9982d2aa276ce56cc50d4
Felipe Dornelas <felipead@gmail.com>
2015-03-02 16:59:19 -0300

    Improved test.

--- test/sync/TestCombinedUpdateEvent.py
+++ test/sync/TestCombinedUpdateEvent.py
@@ -25,35 +25,41 @@ class TestCombinedUpdateEvent:
         assert_that(combined.field_names, contains_inanyorder("a", "b", "c", "d", "e", "f"))
 
     def test_should_get_timestamp_from_most_recent_update(self, identifier):
-        combined = CombinedUpdateEvent(identifier)
-        combined.add_update_event(UpdateEvent(identifier, timestamp=1, field_names={"a"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=5, is_delete=True))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=6, field_names={"g"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=3, field_names={"b", "c", "d"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=2, field_names={"b", "c"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=4, field_names={"e", "f"}))
+        base_timestamp = 1425324825.630933
 
-        assert_that(combined.timestamp, equal_to(6))
+        combined = CombinedUpdateEvent(identifier)
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 1, field_names={"a"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 5, is_delete=True))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 6, field_names={"g"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 3, field_names={"b", "c", "d"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 2, field_names={"b", "c"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 4, field_names={"e", "f"}))
+
+        assert_that(combined.timestamp, equal_to(base_timestamp + 6))
 
     def test_is_delete_if_last_update_is_delete(self, identifier):
+        base_timestamp = 1425324825.630933
+
         combined = CombinedUpdateEvent(identifier)
-        combined.add_update_event(UpdateEvent(identifier, timestamp=1, field_names={"a"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=5, is_delete=True))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=5, field_names={"a"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=3, field_names={"b", "c", "d"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=2, field_names={"b", "c"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=4, field_names={"e", "f"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 1, field_names={"a"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 5, is_delete=True))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 5, field_names={"a"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 3, field_names={"b", "c", "d"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 2, field_names={"b", "c"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 4, field_names={"e", "f"}))
 
         assert_that(combined.is_delete, equal_to(True))
 
     def test_is_not_delete_if_last_update_is_save(self, identifier):
+        base_timestamp = 1425324825.630933
+
         combined = CombinedUpdateEvent(identifier)
-        combined.add_update_event(UpdateEvent(identifier, timestamp=1, field_names={"a"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=5, is_delete=True))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=5, field_names={"a"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=6, field_names={"a"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=3, field_names={"b", "c", "d"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=2, field_names={"b", "c"}))
-        combined.add_update_event(UpdateEvent(identifier, timestamp=4, field_names={"e", "f"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 1, field_names={"a"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 5, is_delete=True))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 5, field_names={"a"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 6, field_names={"a"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 3, field_names={"b", "c", "d"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 2, field_names={"b", "c"}))
+        combined.add_update_event(UpdateEvent(identifier, timestamp=base_timestamp + 4, field_names={"e", "f"}))
 
         assert_that(combined.is_delete, equal_to(False))

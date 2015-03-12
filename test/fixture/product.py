cd24fc0ad9de769f4476f97edd8b705e57887085
Felipe Dornelas <felipead@gmail.com>
2015-03-12 00:18:28 -0300

    Added "timestamp" column to ProductFixture, removed created_at, updated_at.

--- test/fixture/product.py
+++ test/fixture/product.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 import pytest
 
 from app.cassandra_domain.store.AbstractCassandraStore import AbstractCassandraStore
@@ -8,13 +9,12 @@ class ProductFixture:
 
     TABLE_NAME = "product"
 
-    def __init__(self, _id, name, quantity, description):
+    def __init__(self, _id=None, name=None, quantity=None, description=None, timestamp=None):
         self._id = _id
         self.name = name
         self.quantity = quantity
         self.description = description
-        self.created_at = None
-        self.updated_at = None
+        self.timestamp = timestamp
 
     @property
     def id(self):
@@ -27,29 +27,27 @@ class ProductFixture:
 
 class ProductFixtureCassandraStore(AbstractCassandraStore):
 
-    def __init__(self, nodes, keyspace):
-        super(ProductFixtureCassandraStore, self).__init__(nodes, keyspace, ProductFixture.TABLE_NAME)
+    def __init__(self, nodes, keyspace, table):
+        super(ProductFixtureCassandraStore, self).__init__(nodes, keyspace, table)
 
     def create(self, product):
-        product.created_at = datetime.utcnow()
-        product.updated_at = product.created_at
+        product.timestamp = datetime.utcnow()
         statement = self.prepare_statement(
             """
-            INSERT INTO %s (id, name, quantity, description, created_at, updated_at)
-            VALUES (?, ?, ?, ?, ?, ?)
+            INSERT INTO %s (id, name, quantity, description, timestamp)
+            VALUES (?, ?, ?, ?, ?)
             """ % self.table)
-        self.execute(statement, (product.id, product.name, product.quantity, product.description,
-                                 product.created_at, product.updated_at))
+        self.execute(statement, (product.id, product.name, product.quantity, product.description, product.timestamp))
 
     def update(self, product):
-        product.updated_at = datetime.utcnow()
+        product.timestamp = datetime.utcnow()
         statement = self.prepare_statement(
             """
             UPDATE %s
-            SET name=?, quantity=?, description=?, updated_at=?
+            SET name=?, quantity=?, description=?, timestamp=?
             WHERE id=?
             """ % self.table)
-        self.execute(statement, (product.name, product.quantity, product.description, product.updated_at, product.id))
+        self.execute(statement, (product.name, product.quantity, product.description, product.timestamp, product.id))
 
     def delete(self, product):
         statement = self.prepare_statement(
@@ -66,11 +64,11 @@ class ProductFixtureCassandraStore(AbstractCassandraStore):
 # noinspection PyShadowingNames
 @pytest.fixture(scope="session")
 def product_fixture_cassandra_store(cassandra_cluster, cassandra_fixture_keyspace):
-    return ProductFixtureCassandraStore(cassandra_cluster, cassandra_fixture_keyspace)
+    return ProductFixtureCassandraStore(cassandra_cluster, cassandra_fixture_keyspace, ProductFixture.TABLE_NAME)
 
 
 @pytest.fixture(scope="session", autouse=True)
-def setup_product_fixture_cassandra_schema(cassandra_fixture_client, cassandra_log_trigger_name):
+def create_product_fixture_cassandra_schema(cassandra_fixture_client, cassandra_log_trigger_name):
     cassandra_fixture_client.execute("DROP TABLE IF EXISTS %s" % ProductFixture.TABLE_NAME)
     cassandra_fixture_client.execute(
         """
@@ -79,8 +77,7 @@ def setup_product_fixture_cassandra_schema(cassandra_fixture_client, cassandra_l
           name text,
           quantity int,
           description text,
-          created_at timestamp,
-          updated_at timestamp
+          timestamp timestamp
         )
         """ % ProductFixture.TABLE_NAME)
 

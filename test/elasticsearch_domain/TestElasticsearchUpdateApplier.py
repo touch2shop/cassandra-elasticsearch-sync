3755fda981ae18cd25ff5fc303bdef121084732d
Felipe Dornelas <felipead@gmail.com>
2015-03-12 21:05:00 -0300

    Fail if elasticsearch mapping does not store timestamp for document

--- test/elasticsearch_domain/TestElasticsearchUpdateApplier.py
+++ test/elasticsearch_domain/TestElasticsearchUpdateApplier.py
@@ -1,6 +1,6 @@
-import uuid
+from uuid import uuid4
 import pytest
-import time
+from time import time
 from app.core.Identifier import Identifier
 from app.core.Update import Update
 from app.core.UpdateEvent import UpdateEvent
@@ -17,19 +17,40 @@ def update_applier(elasticsearch_client):
 class TestElasticsearchUpdateApplier:
 
     def test_fail_if_index_does_not_exist(self, update_applier):
-        bogus_identifier = Identifier(namespace="invalid", table="product", key=str(uuid.uuid4()))
-        bogus_event = UpdateEvent(identifier=bogus_identifier, timestamp=time.time())
-        bogus_update = Update(bogus_event)
+        bogus_update = self.build_update(namespace="invalid", table="product", key=str(uuid4()),
+                                         timestamp=time())
 
         with pytest.raises(InvalidElasticsearchSchemaException) as e:
             update_applier.apply_updates([bogus_update])
-        assert e.value.identifier == bogus_event.identifier
+        assert e.value.identifier == bogus_update.identifier
 
     def test_fail_if_type_does_not_exist(self, update_applier, elasticsearch_fixture_index):
-        bogus_identifier = Identifier(namespace=elasticsearch_fixture_index, table="invalid", key=str(uuid.uuid4()))
-        bogus_event = UpdateEvent(identifier=bogus_identifier, timestamp=time.time())
-        bogus_update = Update(bogus_event)
+        bogus_update = self.build_update(namespace=elasticsearch_fixture_index, table="invalid", key=str(uuid4()),
+                                         timestamp=time())
 
         with pytest.raises(InvalidElasticsearchSchemaException) as e:
             update_applier.apply_updates([bogus_update])
-        assert e.value.identifier == bogus_event.identifier
+        assert e.value.identifier == bogus_update.identifier
+
+    def build_update(self, namespace, table, key, timestamp):
+        identifier = Identifier(namespace, table, key)
+        event = UpdateEvent(identifier=identifier, timestamp=timestamp)
+        update = Update(event)
+        return update
+
+    def test_fail_if_mapping_does_not_store_timestamp_for_document(self, update_applier,
+                                                                   elasticsearch_client, elasticsearch_fixture_index):
+        _index = elasticsearch_fixture_index
+        _type = "type_without_timestamp"
+        _id = uuid4()
+
+        bogus_mapping = {"_timestamp": {"enabled": True, "store": False}}
+        elasticsearch_client.indices.put_mapping(index=_index, doc_type=_type, body=bogus_mapping)
+        elasticsearch_client.index(index=_index, doc_type=_type, id=_id, body={"foo": "bar"})
+
+        update = self.build_update(namespace=_index, table=_type, key=str(_id), timestamp=time())
+
+        with pytest.raises(InvalidElasticsearchSchemaException) as e:
+            update_applier.apply_updates([update])
+        assert e.value.identifier == update.identifier
+        assert "Could not retrieve timestamp for Elasticsearch document" in e.value.message

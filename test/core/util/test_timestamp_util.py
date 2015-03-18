e67ec7ddb2261a644be9fdaf1f1539fb855c5616
Felipe Dornelas <felipead@gmail.com>
2015-03-18 00:33:03 -0300

    TimestampUtil test coverage.

---
+++ test/core/util/test_timestamp_util.py
@@ -0,0 +1,11 @@
+from app.core.util.timestamp_util import TimestampUtil
+
+
+# noinspection PyMethodMayBeStatic,PyClassHasNoInit
+class TestTimestampUtil:
+
+    def test_seconds_to_milliseconds(self):
+        assert TimestampUtil.seconds_to_milliseconds(10345.123456) == 10345123
+
+    def test_milliseconds_to_seconds(self):
+        assert TimestampUtil.milliseconds_to_seconds(10345123) == 10345.123

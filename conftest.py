e6c7f8bdf00d0d8027591c9b207569fcf691461c
Felipe Dornelas <felipead@gmail.com>
2015-03-20 23:48:30 -0300

    Telling py.test to ignore apache-cassandra-*, since this folder is available when running on Travis-CI.

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 # py.test configuration file.
 
-# This variable tells py.test which files and folders to ignore
-collect_ignore = ["env", "app"]
+# This variable tells py.test which files and folders to ignore.
+# Ignoring "apache-cassandra*" because of Travis-CI.
+collect_ignore = ["env", "app", "apache-cassandra*"]
